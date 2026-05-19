5a4c31b25689204724d9f05ff614e84da75dbce5
michaeltangbcgov <michael.tang@gov.bc.ca>
2026-05-19 15:54:16 -0700

    feat: add province overview page and stylized reporting components for school district dashboard

--- php-bin/resources/views/pages/province.blade.php
+++ php-bin/resources/views/pages/province.blade.php
@@ -1,83 +1,108 @@
 @extends('layout')
 @section('subtitle'){{ $school_district->district_name }} {{ trans('esdr2.sd_heading') }}@endsection
 
+@push('css')
+  <link rel="stylesheet" href="/css/school-directory-results.css">
+  <link href="/css/province.css" rel="stylesheet" type="text/css">
+@endpush
+
 @section('content')
 
-  <section class="slide blue-bg" style="padding-top: 3rem;">
-    <div class="slide-content restrain">
+<style>
+    .directory-masthead { display: none !important; }
+</style>
 
-      <img src="/img/maps/map_sd_099.png" alt="Map of British Columbia" class="key-image thirds">
-
-      <p class="white school-meta">{{ trans('esdr2.prov_results_label') }}</p>
-
-      <h2 class="ministry-blue slide-title">{{ trans('esdr2.british_columbia_heading') }}</h2>
-
-      <p class="school-meta white">
-
-        @if ($school_district->phy_address_line_1)
-          {{ trans('esdr2.ministry_of_education_lable') }}: <a target="_blank" href="https://www.google.ca/maps/place/{{ str_replace(' ', '+', $school_district->present()->formatSchoolDistrictAddress) }}">{{ $school_district->present()->formatSchoolDistrictAddress }}</a>
-        @endif
-
-        @if ($school_district->contact_phone)
-          <br>{{ trans('esdr2.phone_contact_label') }}: <a href="tel:{{ $school_district->present()->concatPhoneNumber }}">{{ $school_district->contact_phone }}</a>
-        @endif
-
-        @if ($school_district->contact_phone_extension)
-          ext. {{ $school_district->contact_phone_extension }}
-        @endif
-
-        @if ($school_district->website)
-          <br>{{ trans('esdr2.website_contact_label') }}: <a href="{{ $school_district->website }}" target="_blank">www2.gov.bc.ca</a>
-        @endif
-
-        @if ($school_district->contact_first_name && $school_district->position && $school_district->contact_last_name)
-          <br>{{ $school_district->position }}: {{ $school_district->contact_first_name }} {{ $school_district->contact_last_name }}
-        @endif
-
-      </p>
-
-    </div>
-  </section>
-
-  <section class="slide aqua-bg">
-    <div class="slide-content restrain">
-
-      <div class="sd-sub-nav">
-
-        @if ($school_district->website)
-          <a class="big button" href="{{ $school_district->website }}" target="_blank">{{ trans('esdr2.provincial_website_lable') }}</a>
-        @endif
-
-        <a id="main-download-report-link" data-sd="099" class="big button" href="/pdf/Enhanced-School-District-Report-for-SD099.pdf">{{ trans('esdr2.download_report_lable') }} (PDF)</a>
-
+<div class="school-district-header">
+  <div class="sd-info-section text-white py-5">
+    <div class="container restrain">
+      <div class="row align-items-center">
+        <div class="col-md-7 col-lg-8 mb-4 mb-md-0">
+          <p class="mb-1 text-uppercase" style="font-size: 0.9rem; font-weight: 500;">{{ trans('esdr2.prov_results_label') }}</p>
+          <h1 class="display-5 fw-bold mb-4">
+              {{ trans('esdr2.british_columbia_heading') }}
+          </h1>
+          
+          <ul class="list-unstyled mb-0" style="font-size: 0.95rem; line-height: 1.6;">
+            @if ($school_district->phy_address_line_1)
+              <li><strong>{{ trans('esdr2.ministry_of_education_lable') }}:</strong> <a class="text-white" target="_blank" href="https://www.google.ca/maps/place/{{ str_replace(' ', '+', $school_district->present()->formatSchoolDistrictAddress) }}">{{ $school_district->present()->formatSchoolDistrictAddress }}</a></li>
+            @endif
+    
+            @if ($school_district->contact_phone)
+              <li><strong>{{ trans('esdr2.phone_contact_label') }}:</strong> <a class="text-white" href="tel:{{ $school_district->present()->concatPhoneNumber }}">{{ $school_district->contact_phone }}</a>
+              @if ($school_district->contact_phone_extension)
+                ext. {{ $school_district->contact_phone_extension }}
+              @endif
+              </li>
+            @endif
+    
+            @if ($school_district->website)
+              <li><strong>{{ trans('esdr2.website_contact_label') }}:</strong> <a class="text-white" href="{{ $school_district->website }}" target="_blank">www2.gov.bc.ca</a></li>
+            @endif
+    
+            @if ($school_district->contact_first_name && $school_district->position && $school_district->contact_last_name)
+              <li><strong>{{ $school_district->position }}:</strong> {{ $school_district->contact_first_name }} {{ $school_district->contact_last_name }}</li>
+            @endif
+          </ul>
+        </div>
+        <div class="col-md-5 col-lg-4 text-center position-relative">
+          <div class="hexagon-map-container">
+            <div class="hexagon-shape-outer"></div>
+            <div class="hexagon-shape"></div>
+            <img src="/img/maps/map_sd_099.png" alt="Map of British Columbia" class="img-fluid hexagon-map" onerror="this.src='/img/maps/map_sd_099.png';">
+          </div>
+        </div>
       </div>
-
-    </div>
-  </section>
-  <div style="margin-top: -5px; padding-top:4rem; padding-bottom: 6rem; background-image: url('{{URL::to('/')}}/img/bg-images/bg-narrow-grey-1.jpg');background-repeat: no-repeat;" class="reports-heading">
-    <div class="restrain">
-      <h3 class="ministry-blue slide-title">
-        {{ trans('esdr2.reports_heading1') }}
-      </h3>
     </div>
   </div>
-  <section class="slide" style="padding-top: 3rem; padding-bottom: 3rem;">
-    @include('components.sd-charts-menu')
-  </section>
-  <div style="margin-top: -5px; padding-top:4rem; padding-bottom: 5rem; background-image: url('{{URL::to('/')}}/img/bg-images/bg-district-foot.jpg');background-repeat: no-repeat;" class="reports-heading">
+
+  <div class="sd-actions-section py-3">
+    <div class="container restrain">
+      <div class="row justify-content-center g-3">
+        @if ($school_district->website)
+        <div class="col-12 col-md-auto text-center">
+          <a href="{{ $school_district->website }}" class="btn btn-navy px-5 py-2 fw-bold text-white shadow-sm w-100" target="_blank">{{ trans('esdr2.provincial_website_lable') }}</a>
+        </div>
+        @endif
+        <div class="col-12 col-md-auto text-center">
+          <a id="main-download-report-link" data-sd="099" href="/pdf/Enhanced-School-District-Report-for-SD099.pdf" class="btn btn-navy px-5 py-2 fw-bold text-white shadow-sm w-100">{{ trans('esdr2.download_report_lable') }} (PDF)</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="sd-reports-section py-5 bg-light position-relative overflow-hidden">
+    <!-- Background plus graphic -->
+    <div class="position-absolute d-none d-lg-block" style="right: 5%; top: 45%; color: #b5ded8; font-family: monospace; font-size: 2rem; letter-spacing: 50px;">
+        + + + +<br><br>+ + + +
+    </div>
+
+    <div class="container restrain position-relative z-1">
+      <div class="row mb-5 align-items-center">
+        <div class="col-12 d-flex align-items-center">
+          <img src="/img/hero_hexagons.png" alt="" class="me-3" style="width: 70px; filter: grayscale(1); opacity: 0.15;">
+          <h2 class="text-navy mb-0 fs-3">{{ trans('esdr2.reports_heading1') }}</h2>
+        </div>
+      </div>
+      
+      @include('components.sd-charts-menu')
+      
+    </div>
+  </div>
+</div>
+  <div class="reports-heading reports-heading-foot">
   <div class="restrain">
     <div class="row">
-      <div class="col-sm-12 col-md-5">
+      <div class="col-12 col-md-5">
         <img src="/img/reports-pic.png" alt="picture of reports" width="auto" height="190px">
       </div>
-      <div class="col-sm-12 col-md-7">
+      <div class="col-12 col-md-7">
         <p><h3 class="dark-blue">FSA Item Analysis</h3></p>
-        <p><img class="green-bar" style="margin:0px; float: left" src="{{URL::to('/')}}/img/green-bar-2.png" alt=""/><br></p>
+        <p><img class="green-bar m-0 float-start" src="{{URL::to('/')}}/img/green-bar-2.png" alt=""/><br></p>
         <p>Reports for educators to help interpret and understand<br>
           students' results for the provincial Grades 4 and 7 Foundation<br>
           Skills Assessment.
         </p>
-        <p><a class="btn btn-primary btn-lg" style="border-radius:0px;" href="/fsa/index.html">View the Data+</a></p>
+        <p><a class="btn btn-primary btn-lg rounded-0" href="/fsa/index.html">View the Data+</a></p>
       </div>
     </div>
   </div>
--- php-bin/resources/views/pages/sd-report.blade.php
+++ php-bin/resources/views/pages/sd-report.blade.php
@@ -2,29 +2,44 @@
 @extends('layout')
 @section('content')
 
-@include('components.sd-meta')
-<script>
-$(function() {
-  // bind change event to select
-  $('#dynamic_select').on('change', function() {
-    var url = $(this).val(); // get selected value
-    if (url) { // require a URL
+<link rel="stylesheet" href="/css/school-directory-results.css">
 
-      window.location = url; // redirect
-    }
-    return false;
-  });
-});
-</script>
-  <section class="slide aqua-bg options-row">
-    <div class="slide-content restrain">
-    <div class="form-group">
-      <!-- <label class="control-label col-sm-offset-2 col-sm-2" for="company">Company</label> -->
+<style>
+    .directory-masthead { display: none !important; }
+    #report-subtitle { color: #b5ded8 !important; }
+</style>
+
+<div class="school-district-header">
+  <div class="sd-info-section text-white py-5">
+    <div class="container restrain">
+      <div class="row align-items-center">
+        <div class="col-md-7 col-lg-8 mb-4 mb-md-0">
+          <p class="mb-1 text-uppercase" style="font-size: 0.9rem; font-weight: 500;">School District Report</p>
+          <h1 class="display-5 fw-bold mb-4">
+              {{ Helper::formatCityForHuman($school_district->district_name) }} 
+              @if ($school_district->sd != '099') ({{ Helper::removeLeadingZeros($school_district->sd) }}) @endif
+          </h1>
+          
+          <div class="report-meta-container mt-4">
+            @include('components.report-meta-descriptions')
+          </div>
+        </div>
+        <div class="col-md-5 col-lg-4 text-center position-relative">
+          <div class="hexagon-map-container">
+            <div class="hexagon-shape-outer"></div>
+            <div class="hexagon-shape"></div>
+            <img src="/img/maps/map_sd_{{ $school_district->sd }}.png" alt="Map of {{ $school_district->district_name }}" class="img-fluid hexagon-map" onerror="this.src='/img/maps/map_sd_099.png';">
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="sd-sub-nav">
+  </div>
 
-      <div class="col-sm-12 col-md-8 more-reports">
-          <select id="dynamic_select" class="form-control report-select">
+  <div class="sd-actions-section py-3">
+    <div class="container restrain">
+      <div class="row justify-content-center align-items-center g-3">
+        <div class="col-12 col-md-8 more-reports">
+          <select id="dynamic_select" class="form-select report-select">
             <option value="" selected>Select another report</option>
             <option value="/school-district/{{ $school_district->sd }}/report/contextual-information">Demographic Information</option>
             <option value="/governance/{{ $school_district->sd }}">Key contacts</option>
@@ -38,10 +53,10 @@ $(function() {
             <option value="/school-district/{{ $school_district->sd }}/report/post-secondary-career-prep">Post-Secondary and Career Preparation</option>
             <option value="/school-district/{{ $school_district->sd }}/report/transition-to-post-secondary">Transition to B.C. Post-Secondary</option>
           </select> 
-          
-      </div>
-      <div class="col-sm-12 col-md-4 more-reports">
-        <a class="view-all" href="/school-district/{{ $school_district->sd }}">View All Reports</a>
+        </div>
+        <div class="col-12 col-md-4 more-reports text-center text-md-start">
+          <a class="btn btn-navy px-4 py-2 fw-bold text-white shadow-sm w-100" href="/school-district/{{ $school_district->sd }}">View All Reports</a>
+        </div>
       </div>
     
         @php
@@ -61,12 +76,25 @@ $(function() {
           );
           $report_headings_keys = array_keys($report_headings);
         @endphp
-      </div>
-
     </div>
-  </section>
+  </div>
+</div>
 
-  <section style="padding-top: 3rem;" class="slide fill-viewport">
+<script>
+$(function() {
+  // bind change event to select
+  $('#dynamic_select').on('change', function() {
+    var url = $(this).val(); // get selected value
+    if (url) { // require a URL
+
+      window.location = url; // redirect
+    }
+    return false;
+  });
+});
+</script>
+
+  <section class="slide fill-viewport pt-5 bg-light">
     <div class="slide-content restrain">
 
       @if ($report_slug == 'contextual-information')
@@ -118,7 +146,7 @@ $(function() {
         
         <h3 class="slide-title light-blue">{{ trans('esdr2.students_entering_school_heading') }}</h3>
         <iframe scrolling="no" id="frameId-8" class="tableau-embed" src="//public.tableau.com/views/ESDR1/4_EDI?:showVizHome=no&:display_share=no&:embed=true&:toolbar=no&:device=desktop&SD={{ $school_district->sd }}"></iframe>
-        <div style="background:white;margin-top: -41px;position:absolute;height: 27px;width: 50%;"></div> 
+        <div class="tableau-overlay-half"></div> 
       @endif
 
       @if ($report_slug == 'completion-rates')
@@ -356,7 +384,7 @@ $(function() {
         <h3 class="slide-title light-blue">{{ trans('esdr2.prov_exam_heading') }}</h3>
         @include('components.chart-legend')
         <iframe scrolling="no" id="frameId-12" class="tableau-embed" src="//public.tableau.com/views/ESDR1/13_Assessments?:showVizHome=no&:display_share=no&:embed=true&:toolbar=no&:device=desktop&SD={{ $school_district->sd }}"></iframe>
-        <div style="background:white;margin-top: -41px;position:absolute;height: 27px;width: 100%;"></div> 
+        <div class="tableau-overlay-full"></div> 
       @endif
       @if ($report_slug == 'grad-assess')
         @include('components.chart-legend')
@@ -501,6 +529,9 @@ $(function() {
 @endsection
 
 @section('subtitle'){{ $school_district->district_name }} {{ trans('esdr2.sd_heading') }}: {{ $report_headings[$sd_report_slugs[$current_report_index - 0]] }}@endsection
+@push('css')
+  <link href="/css/sd-report.css" rel="stylesheet" type="text/css">
+@endpush
 @push('scripts') 
 	<script src="https://vega.github.io/vega/assets/promise.min.js"></script>
 	<script src="https://vega.github.io/vega/assets/symbol.min.js"></script>
